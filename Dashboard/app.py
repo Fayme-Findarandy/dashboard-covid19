ed3d18c0dd886fff168f8e72d61c5d4753c0f4b1
Fayme-Findarandy <184240034@std.ulbi.ac.id>
2026-05-01 17:23:26 +0700

    Update app.py

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -21,6 +21,7 @@ if menu == "Home":
     bar_chart1(df_filtered)
     bar_chart2(df_filtered)
     map_chart(df_filtered, year)
+    
 
 elif menu == "Data":
     judul()
@@ -29,3 +30,6 @@ elif menu == "Data":
     location = select_location(df)
     df_filtered = filter_data(df, year, location)
     show_data(df_filtered)  
+
+st.markdown("---")
+st.markdown("© 2026 Fayme Findarandy / 184240034. All Rights Reserved.")
